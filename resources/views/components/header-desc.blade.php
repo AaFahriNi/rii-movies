8699e42b10095b76ba11c51be00ae286e50e9bda
Alfachri Diantoro <144247777+AaFahriNi@users.noreply.github.com>
2024-09-14 15:14:42 +0700

    Update header-desc.blade.php

--- resources/views/components/header-desc.blade.php
+++ resources/views/components/header-desc.blade.php
@@ -5,7 +5,7 @@
     </a>
       <!-- Form pencarian -->
       <div class="search-container">
-        <form action="{{ route('movie.search') }}" method="GET" class="search-form">
+        <form action="https://moviesriii.up.railway.app" method="GET" class="search-form">
             <div class="input-container">
                 <i class='bx bx-search search-icon'></i>
                 <input type="text" name="query" id="search-input" placeholder="Search..." class="search-input">

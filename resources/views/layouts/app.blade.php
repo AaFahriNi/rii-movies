5aa5fec48b970a91921d3063994b9fc0ef19e93e
alfachri_i <alfachrihp@gmail.com>
2024-09-14 13:28:32 +0700

    v1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Movies</title>
-    <link href="{{ asset('img/icon.png') }}" rel="icon">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://moviesriii.up.railway.app/img/icon.png" rel="icon">
+    <link rel="stylesheet" href="https://moviesriii.up.railway.app/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 </head>
@@ -19,6 +19,6 @@
     @include('components.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
+    <script src="https://moviesriii.up.railway.app/js/main.js"></script>
 </body>
 </html>
--- resources/views/layouts/desc-page.blade.php
+++ resources/views/layouts/desc-page.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Movies</title>
-    <link href="{{ asset('img/icon.png') }}" rel="icon">
+    <link href="https://moviesriii.up.railway.app/img/icon.png" rel="icon"" rel="icon">
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet "href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="{{ asset('css/desc.css') }}">
+    <link rel="stylesheet" href="https://moviesriii.up.railway.app/css/desc.css">
     <link rel="stylesheet "href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
 </head>
 <body>
